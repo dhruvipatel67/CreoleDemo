7d156d2546c5fc68d8f4c5181321fd5163ea84d5
dhruvipatel67 <167097708+dhruvipatel67@users.noreply.github.com>
2025-02-25 19:43:28 +0530

    W2SUPERGLOBALS_PHP
    SUPERGLOBALS PRACTICAL PERFORM PHP FILE.

---
+++ PRACTICALPHP/superglobals.php
@@ -0,0 +1,116 @@
+<!-- <?php
+        $x = 75;
+        function f()
+        {
+            echo $GLOBALS['x'];
+        }
+
+        f()
+        ?> -->
+
+<!-- <?php
+        $x = 75;
+
+        function my()
+        {
+            global $x;
+            echo $x;
+        }
+        my()
+        ?> -->
+
+<!-- <?php
+        $x = 100;
+
+        echo $GLOBALS["x"];
+        echo $x;
+        ?> -->
+
+<!-- <?php
+        function myfunction()
+        {
+            $GLOBALS["x"] = 100;
+        }
+
+        myfunction();
+
+        echo $GLOBALS["x"];
+        echo $x;
+        ?> -->
+
+<!-- <?php
+        echo $_SERVER['PHP_SELF'];
+        echo "<br>";
+        echo $_SERVER['SERVER_NAME'];
+        echo "<br>";
+        echo $_SERVER['HTTP_HOST'];
+        echo "<br>";
+        echo $_SERVER['HTTP_REFERER'];
+        echo "<br>";
+        echo $_SERVER['HTTP_USER_AGENT'];
+        echo "<br>";
+        echo $_SERVER['SCRIPT_NAME'];
+        ?> -->
+
+
+<!-- <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_REQUEST['fname']);
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?> -->
+
+<!-- <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+</form> -->
+
+<!-- <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = htmlspecialchars($_REQUEST['fname']);
+            if (empty($name)) {
+                echo "Name is empty";
+            } else {
+                echo $name;
+            }
+        }
+        ?>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?> -->
+
+
+
+<!-- <?php
+        echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+        ?> -->
+
+<!-- <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['fname']);
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?> -->
+
+<?php
+$arr = getenv();
+foreach ($arr as $key => $val)
+    echo "$key=>$val";
+?>
